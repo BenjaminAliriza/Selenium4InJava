3fbb1a88c71f2cbb54b1ee4928420da32f314455
Benjamin Aliriza <baliriza@dvt.co.za>
2022-07-04 13:54:26 +0200

    Chapter 3 begun

---
+++ src/test/java/chapter3/RelativeLocators.java
@@ -0,0 +1,35 @@
+package chapter3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.testng.annotations.*;
+
+public class RelativeLocators {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void testRelativeLocator(){
+        WebElement loginPanel = driver.findElement(By.id("logInPanelHeading"));
+        WebElement credentials = driver.findElement(RelativeLocator.with(By.tagName("span")).above(loginPanel));
+        System.out.println(credentials.getText());
+        //7:29 in the video
+    }
+}
