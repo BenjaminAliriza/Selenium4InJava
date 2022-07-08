097d3e63f08e042e63d766616358ac80b9f325d8
Benjamin Aliriza <baliriza@dvt.co.za>
2022-07-08 11:40:31 +0200

    network conditions complete and WHOLE project complete

--- src/test/java/chapter7/Geolocation.java
+++ src/test/java/chapter7/Geolocation.java
@@ -1,7 +1,6 @@
 package chapter7;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.v101.emulation.Emulation;
